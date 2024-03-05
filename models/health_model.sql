011c94aae494eeab23b6ecd0ee6fd945fa3ad4fd
ghvimieiro@gmail.com <ghvimieiro@gmail.com>
2024-03-05 23:50:01 +0000

    update

--- models/health_model.sql
+++ models/health_model.sql
@@ -1,22 +1,22 @@
 {{ config(materialized = 'table')}}
 
-with HEALTH as (
+with health as (
     select * 
     from {{ ref('base_health')}}
-)
+),
 
-health_model as (
+health_full as (
     select
-        HEALTH._AIRBYTE_RAW_ID,
-        HEALTH.LOCATION_KEY,
-        iff(HEALTH.LIFE_EXPECTANCY = null, 0, HEALTH.LIFE_EXPECTANCY) as LIFE_EXPECTANCY,
-        iff(HEALTH.NURSES_PER_1000 = null, 0, HEALTH.NURSES_PER_1000) as NURSES_PER_1000,
-        iff(HEALTH.PHYSICIANS_PER_1000 = null, 0, HEALTH.PHYSICIANS_PER_1000) as PHYSICIANS_PER_1000,
-        iff(HEALTH.POLLUTION_MORTALITY_RATE = null, 0, HEALTH.POLLUTION_MORTALITY_RATE) as POLLUTION_MORTALITY_RATE,
-        iff(HEALTH.HOSPITAL_BEDS_PER_1000 = null, 0, HEALTH.HOSPITAL_BEDS_PER_1000) as HOSPITAL_BEDS_PER_1000,
-        iff(HEALTH.ADULT_FEMALE_MORTALITY_RATE = null, 0, HEALTH.ADULT_FEMALE_MORTALITY_RATE) as ADULT_FEMALE_MORTALITY_RATE,
-        iff(HEALTH.ADULT_MALE_MORTALITY_RATE = null, 0, HEALTH.ADULT_MALE_MORTALITY_RATE) as ADULT_MALE_MORTALITY_RATE
-    from HEALTH
+        health._AIRBYTE_RAW_ID,
+        health.LOCATION_KEY,
+        iff(health.LIFE_EXPECTANCY = null, 0, health.LIFE_EXPECTANCY) as LIFE_EXPECTANCY,
+        iff(health.NURSES_PER_1000 = null, 0, health.NURSES_PER_1000) as NURSES_PER_1000,
+        iff(health.PHYSICIANS_PER_1000 = null, 0, health.PHYSICIANS_PER_1000) as PHYSICIANS_PER_1000,
+        iff(health.POLLUTION_MORTALITY_RATE = null, 0, health.POLLUTION_MORTALITY_RATE) as POLLUTION_MORTALITY_RATE,
+        iff(health.HOSPITAL_BEDS_PER_1000 = null, 0, health.HOSPITAL_BEDS_PER_1000) as HOSPITAL_BEDS_PER_1000,
+        iff(health.ADULT_FEMALE_MORTALITY_RATE = null, 0, health.ADULT_FEMALE_MORTALITY_RATE) as ADULT_FEMALE_MORTALITY_RATE,
+        iff(health.ADULT_MALE_MORTALITY_RATE = null, 0, health.ADULT_MALE_MORTALITY_RATE) as ADULT_MALE_MORTALITY_RATE
+    from health
 )
 
-select * from health_model
+select * from health_full

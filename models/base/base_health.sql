b0ae08d3426a721c83e46991fef6f1849b9f97d2
ghvimieiro@gmail.com <ghvimieiro@gmail.com>
2024-03-05 14:38:06 +0000

    fix duplicate columns

---
+++ models/base/base_health.sql
@@ -0,0 +1,15 @@
+with base_health as (
+    select
+        _AIRBYTE_RAW_ID,
+        LOCATION_KEY,
+        NURSES_PER_1000,
+        PHYSICIANS_PER_1000,
+        POLLUTION_MORTALITY_RATE,
+        LIFE_EXPECTANCY,
+        HOSPITAL_BEDS_PER_1000,
+        ADULT_FEMALE_MORTALITY_RATE,
+        ADULT_MALE_MORTALITY_RATE
+    from {{ source('raw_health', 'HEALTH')}}
+)
+
+select * from base_health
